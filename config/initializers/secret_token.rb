dadb2bc2892f148d809b04ed75603fa3d47f933f
Shana Weitzen <shanaweitzen@gmail.com>
2013-08-04 12:46:14 -0700

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ooply::Application.config.secret_key_base = 'a41124d4377044398326b556049cb5c6948f73f735d727d78bc0d9052847c6a95bb263dc2a649eb043d806bed0debce559a0cde35b29cdf90f1c70292597a8c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ooply::Application.config.session_store :cookie_store, key: '_ooply_session'
