4ce53a3c7dd8af0bf4569ed3065e42d13132c4e3
Shana Weitzen <shanaweitzen@gmail.com>
2013-08-14 10:21:07 -0700

    flip cards

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,18 @@
+$(document).ready(function(){
+	$('.front').click(function(){
+			$(this).parents('.card_holder').addClass('flip')
+			console.log(this) //this is a ref to the card_holder i clicked
 
-// $('.card_holder').click(function(){
-// 	$(this).flip();
-// });
+	});
 
-// $('.card_holder').click(function(){
-//         $(this).find('.card').addClass('flipped').mouseleave(function(){
-//             $(this).removeClass('flipped');
-//         });
-//         return false;
-//     });
+	$('.back').click(function(){
+			$(this).parents('.card_holder').removeClass('flip')
+			console.log(this) //this is a ref to the card_holder i clicked
+
+	});
+		
+});
+
+
+
 
