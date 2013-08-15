06b06d4f35a4b84194f8b0d591bbd1dd0d8a88c1
Shana Weitzen <shanaweitzen@gmail.com>
2013-08-14 18:11:42 -0700

    make flashcards flip and get the next card

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,16 +3,27 @@ $(document).ready(function(){
 			$(this).parents('.card_holder').addClass('flip')
 			console.log(this) //this is a ref to the card_holder i clicked
 
-	});
+	}); 
 
 	$('.back').click(function(){
 			$(this).parents('.card_holder').removeClass('flip')
 			console.log(this) //this is a ref to the card_holder i clicked
 
-	});
-		
-});
+	}); //ends the .back click function
 
+	$('.next').click(function(){
+			card = $('.card_holder').last().detach();
+			$('.card_holder').first().before(card)
+	});
+}); // ends the document.ready function 
+
+
+
+// card # 
+// $(#cardA).hide();
+// $(#cardB).show();
+
+// next week make a branch 
 
 
 
