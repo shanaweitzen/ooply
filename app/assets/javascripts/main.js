a87d671d07c09e3d9b408e994ccc5ce42406fdf5
Shana Weitzen <shanaweitzen@gmail.com>
2013-08-18 17:13:14 -0700

    modify buttons and header

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,39 +3,19 @@ $(document).ready(function(){
 			$(this).parents('.card_holder').addClass('flip')
 			console.log(this) //this is a ref to the card_holder i clicked
 
-<<<<<<< HEAD
-	}); 
-=======
 	});
->>>>>>> 345ef000ad5c518fc14cd0fdc8bdd25cdb9836c5
 
 	$('.back').click(function(){
 			$(this).parents('.card_holder').removeClass('flip')
-			console.log(this) //this is a ref to the card_holder i clicked
-
-<<<<<<< HEAD
-	}); //ends the .back click function
-
-	$('.next').click(function(){
+			console.log(this) 
+	}); //ends the .back click fxn
+		
+		$('.next').click(function(){
 			card = $('.card_holder').last().detach();
 			$('.card_holder').first().before(card)
-	});
-
-	$('.back-button').click(function(){
+	}); // ends next card fxn
+		$('.back-button').click(function(){
 		card = $('.card_holder').first().detach();
 		$('.card_holder').last().after(card)
 	});
-}); // ends the document.ready function 
-
-
-
-
-=======
-	});
-		
-});
->>>>>>> 345ef000ad5c518fc14cd0fdc8bdd25cdb9836c5
-
-
-
-
+}); // ends document ready 
